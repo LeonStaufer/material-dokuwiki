139e53a03cfedb870144a05fb2a15c8a9e102008
Leon Staufer <leon.staufer@googlemail.com>
2018-02-06 18:45:22 +0100

    added feedback option to sidebar

--- sidebar.php
+++ sidebar.php
@@ -6,6 +6,31 @@
  * for example you can choose any of icon available at https://material.io/icons/
  *
  */
+
+/*
+ * Choose if you want to have a feedback form and if it should include technical information.
+ */
+$feedbackForm = true;
+$technical = true;
+
+/*
+* You can customize the feedback form below.
+*/
+
+$email = "leon.staufer@gmail.com";
+$subjectLine = "Feedback for Website";
+$body = "Thank you so much for taking the time to write feedback. We really appreciate it :) \n\n [your message] \n\n\n You can ignore all the technical information below. It only helps us track down what the problem might be.";
+
+$feedbackLink = "mailto:".$email."?subject=".rawurlencode($subjectLine)."&body=".rawurlencode($body);
+$technicalDump = "REDIRECT_STATUS: " .   $_SERVER["REDIRECT_STATUS"] . "\n".
+    "HTTP_HOST: " .  $_SERVER["HTTP_HOST"] . "\n".
+    "HTTP_X_REAL_IP: " .  $_SERVER["HTTP_X_REAL_IP"] . "\n".
+    "HTTP_USER_AGENT: " .  $_SERVER["HTTP_USER_AGENT"] . "\n".
+    "HTTP_ACCEPT: " .  $_SERVER["HTTP_ACCEPT"] . "\n".
+    "HTTP_ACCEPT_ENCODING: " .   $_SERVER["HTTP_ACCEPT_ENCODING"] . "\n".
+    "HTTP_ACCEPT_LANGUAGE: " .  $_SERVER["HTTP_ACCEPT_LANGUAGE"] . "\n".
+    "HTTP_X_REAL_IP: " .  $_SERVER["HTTP_X_REAL_IP"];
+if($technical) $feedbackLink .= rawurlencode("\n\n====PLEASE DO NOT DELETE=====\nPage:".$INFO['id']."\nPerm:".$INFO['perm']."\nUser:".$INFO['client']."\nMobile:".$INFO['ismobile']."\nAction:".$ACT."\n====MORE TECH INFORMATION=====\n".$technicalDump);
 ?>
 
 <a class="mdl-navigation__link" href="<?php echo DOKU_BASE . "doku.php?id=wiki:dokuwiki" ?>">
@@ -15,6 +40,11 @@
     <i class="material-icons" role="presentation">done</i>
     Second link</a>
 <div class="mdl-layout-spacer"></div>
+<?php if($feedbackForm): ?>
+    <a class="mdl-navigation__link" href="<?php echo $feedbackLink ?>" target="_blank">
+        <i class="material-icons" role="presentation">feedback</i>
+        Feedback</a>
+<?php endif; ?>
 <a class="mdl-navigation__link" href="<?php echo DOKU_BASE . "doku.php?do=media" ?>">
     <i class="material-icons" role="presentation">perm_media</i>
     Media Manager</a>
