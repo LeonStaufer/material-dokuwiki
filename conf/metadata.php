2cd8f1ef96b9025999c2f597d1d91a2fbc20fe55
Leon Staufer <leon.staufer@googlemail.com>
2018-08-04 21:56:01 +0200

    added configuration options

---
+++ conf/metadata.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Metadata for configuration options
+ */
+
+$meta['dokuwikiSidebar'] = array('onoff');
+
+$meta['feedbackForm'] = array('onoff');
+$meta['technicalFeedbackForm'] = array('onoff');
+$meta['feedbackEmail'] = array('email');
+$meta['feedbackSubjectLine'] = array('string');
+$meta['feedbackBody'] = array('string');
+
+$meta['hiddenActions']  = array('multicheckbox','_choices' => array('edit','revs','backlink','export_pdf','top'));
+
+$meta['protrudingDrawer'] = array('onoff');
